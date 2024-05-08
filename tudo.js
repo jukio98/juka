9ef2d518269d33b251ed7cfe6d391a00b8e776f4
jukio98 <113953323+jukio98@users.noreply.github.com>
2024-05-08 03:08:51 -0300

    Atualizar o tudo.js

--- tudo.js
+++ tudo.js
@@ -4,8 +4,7 @@ videos=[
 "https://drive.google.com/file/d/1JLxYncDGAzxISM7zAHQ6ZF0R6khQvRyI/preview",
 "https://drive.google.com/file/d/1JQkFPoeZKPe7FyFXfijhMujuARCpEPAJ/preview",
 "https://drive.google.com/file/d/1JVQP1ivMF-gLQvQG_4dXfCsJwF4tLbU6/preview",
-"https://drive.google.com/file/d/1JVQP1ivMF-gLQvQG_4dXfCsJwF4tLbU6/preview",
-"https://drive.google.com/file/d/1TQ-gvX7mxHlLUHpC7jKw14wbO4J7_qd9/preview"
+"https://drive.google.com/file/d/1JVQP1ivMF-gLQvQG_4dXfCsJwF4tLbU6/preview"
 ]
 
 
