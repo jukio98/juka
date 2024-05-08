6cdb711536d130d95045df4f97158759067ed790
jukio98 <113953323+jukio98@users.noreply.github.com>
2024-05-08 02:43:51 -0300

    Atualizar o tudo.js

--- tudo.js
+++ tudo.js
@@ -1,1 +1,17 @@
+videos=[
+
+"https://drive.google.com/file/d/1JFHPgPHjryV3elztTiqAwVinLYtIevdc/preview",
+"https://drive.google.com/file/d/1JLxYncDGAzxISM7zAHQ6ZF0R6khQvRyI/preview",
+"https://drive.google.com/file/d/1JQkFPoeZKPe7FyFXfijhMujuARCpEPAJ/preview",
+"https://drive.google.com/file/d/1JVQP1ivMF-gLQvQG_4dXfCsJwF4tLbU6/preview",
+"https://drive.google.com/file/d/1JVQP1ivMF-gLQvQG_4dXfCsJwF4tLbU6/preview",
+"https://drive.google.com/file/d/1TQ-gvX7mxHlLUHpC7jKw14wbO4J7_qd9/preview"
+]
+
+
+
+
+
+
+
 lista_produtos=["LINGUIÇA SEARA_06/03/2024","; LINGUIÇA PERDIGAO_29/03/2024","; LINGUIÇA SEARA_24/03/2024","; LINGUIÇA SEARA_27/03/2024","; LINGUIÇA SEARA_29/03/2024","; LINGUIÇA PERDIGAO_04/04/2024","; PRESUNTO SADIA_24/03/2024",""]
