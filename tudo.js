55fd262187b8d4da7bbce045f30024ef0b3f6c69
jukio98 <113953323+jukio98@users.noreply.github.com>
2024-06-10 03:40:40 -0300

    Atualizar o tudo.js

--- tudo.js
+++ tudo.js
@@ -1,3 +1,51 @@
+
+produtos=[
+{
+dia:"14/06/24",
+ms:06,
+nome:"BATATA SADIA 300G",
+codigo:"78910203040"
+},
+
+{
+dia:"13/07/24",
+ms:07,
+nome:"BATATA SADIA 300G",
+codigo:"78910203040"
+},
+
+
+{
+dia:"11/06/24",
+ms:06,
+nome:"BATATA SADIA 300G",
+codigo:"78910203040"
+},
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 videos=[
 
 "https://drive.google.com/file/d/1JFHPgPHjryV3elztTiqAwVinLYtIevdc/preview",
