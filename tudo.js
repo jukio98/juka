dabf299aa341fd607926e0acee0902093287b958
jukio98 <113953323+jukio98@users.noreply.github.com>
2024-06-10 15:17:56 -0300

    Atualizar o tudo.js

--- tudo.js
+++ tudo.js
@@ -22,6 +22,22 @@ nome:"BATATA SADIA 300G",
 codigo:"78910203040"
 },
 
+
+{
+dia:"10/06/24",
+ms:06,
+nome:"BATATA SEARA 300G",
+codigo:"78910203040"
+},
+
+
+{
+dia:"11/76/24",
+ms:06,
+nome:"BATATA FRIMESA 300G",
+codigo:"78910203040"
+},
+
 ]
 
 
