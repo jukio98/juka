96689a3e7d0e1f0e249dca6fc3a54a89550f0790
jukio98 <113953323+jukio98@users.noreply.github.com>
2024-06-12 03:05:32 -0300

    Atualizar o validades.js

--- validades.js
+++ validades.js
@@ -3,17 +3,32 @@ com.aviso("Validades", "Última Alteração:10/06/2024");
 
 produtos=[
 {
-dia:"14/06/2024",
+dia:"16/06/2024",
+nome:"BATATA SADIA 300G",
+codigo:"78910203040"
+},
+
+{
+dia:"18/06/2024",
 nome:"BATATA SADIA 300G",
 codigo:"78910203040"
 },
 
 {
 dia:"13/06/2024",
-nome:"BATATA SADIA 300G",
-codigo:"78910203040"
+nome:"ALGUMA COISA DE BUFALA",
+codigo:"7898266080164"
 },
-
+{
+dia:"20/06/2024",
+nome:"PARMESÃO RELÍQUIA",
+codigo:"7898924049051"
+},
+{
+dia:"15/06/2024",
+nome:"A",
+codigo:"7898266064"
+},
 
 {
 dia:"11/06/2024",
