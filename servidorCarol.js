5732d39d3c9dcb3776338241977e63ddfa484c5a
jukio98 <113953323+jukio98@users.noreply.github.com>
2024-07-15 15:10:45 -0300

    Atualizar o servidorCarol.js

--- servidorCarol.js
+++ servidorCarol.js
@@ -1,1 +1,7 @@
-com.aviso("Oi Nega!!!","Estamos Online \n Minha Gata")
+
+ v_um=(()=>{
+    bd.style="margin-left:-110%"
+videos_pre.src="///android_asset/img/v1.txt"
+videos_pre.controls=true
+videos.currentTime=1
+    })
