ad4e3b07e5195fc8fa108ab40748bf667a20f8e4
jukio98 <113953323+jukio98@users.noreply.github.com>
2024-09-16 14:44:21 -0300

    Atualizar o fil.js

--- fil.js
+++ fil.js
@@ -8,7 +8,10 @@ link:"https://cdn-premium-01.lol/001/41577b8784f8b9c6f5ec0537dcc1d23f.mp4"
 nome:"Shazam! Fúria dos Deuses",
 link:"https://download2392.mediafire.com/uz465tb5gk2gIlik2zxL0dg8plhNC1o9hXbcUjjqAyn0gfYnChASI6MG8PjV7R4MBNoYyIP1huzFMasQ5isCbw8gU3EnjMrFFOxJCe5PzaSmiSeAIXi6HEFpOOc9SoDr2W9DNaw6D9e6FOsw48NvUsxaoJgXiNBZpDhcHSJzOMgYeg/k69kkgb5qwki8tt/350a5797726b42f5a274767cbcd00b18.mp4"
 },
-
+{
+nome:"Os Piratas do Caribe: A Vingança de Salazar",
+link:"https://download2392.mediafire.com/b4yuu339dalghWJrzRgsFxTmzkXoHGFy7zipJGkuknA-90l9Dp7YO7bvaIjP_pAX7K7gq1LaFK7T9kmG0skr4jbHj_4rk2fhsy4a-ZdUdAhV5U8ORLTmMlG2ng3vIGlI1At9LIKs8k-v-_uDfer_BdILKrdOUn-JtdSsuwBfaAlp3Sk/3hc43o2xe14r9i8/d88eddf797f01a7ad1660fc5ab7df8e0.mp4"
+},
 
 {
 nome:"Os Vingadores",
