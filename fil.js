29019c229f5d05ce2a81a7c9b9919c6cd495787c
jukio98 <113953323+jukio98@users.noreply.github.com>
2024-09-17 00:05:36 -0300

    Atualizar o fil.js

--- fil.js
+++ fil.js
@@ -10,12 +10,6 @@ link:"https://cdn-premium-01.lol/001/226c74751f9dde6741abfe4b619d3853.mp4"
 
 
 {
-nome:"pantera negra",
-link:"https://download2391.mediafire.com/7sqinl47qw7g0jES-1N5RwWgktGsYDiQ4j5QxIRm0E9R_Msn9usYT7hu_-SiTnk1_qSHtXMjRFB-dvRm_wDcvRed_S3wTnj2hiJTVnsCKRaLuTbYESztEF4ciqcsLm0KDBo4N0lDDFKlscdRYCPMDBdrjw1o5V2t5RzCl6sDn8EylTE/1hl1wddqee4gxr2/efd88a7be576c3f1627c493984373be3.mp4"
-},
-
-
-{
 nome:"Minions- a origem de gru.",
 link:"https://download2390.mediafire.com/olj9uoon4gsghyKQBZx4R3mXtGZdro7nxZTX_MZj2M1cBPihYWA_-1F9eTYFH1ih9ETwbUKJMEaHEr07R7i7KXj2GkAirOt65uvS9B-pzo_GUgA4w9aLQ-iyjWynVyKzINGzZPT7NhpHukDenkm2xbz_RSpY0xpNV7TDP0tEh1skjGE/iyrrihkc6tyyqt7/0a5c69f995be3d7f9a22f94416947d33.mp4"
 },
@@ -53,6 +47,10 @@ link:"https://download2391.mediafire.com/h58fttnkurigjQJ6PVKaGBfZHaubegJaMCTmZhT
 {
 nome:"Godzilla vs Kong - o novo império",
 link:"https://download2390.mediafire.com/fdfelf2scoggViv4ZKQbZyqTLtbF5fs0V5xzbIUomG38vH5rFmoeG_an1Oo5M15UP4e2m5oqE-KZrqYxbna3xB-z0qRcoETJtwS1MO6EgdBGHjJquViF_id7nSvvKJwqjy8tdY3ti74N-IMAaOvGaVckAzVACQl_JBkBlqRSFiNYJ3Y/uj7wk3mmo6zzsl2/897c858b55384982b03ca0e5cdf6b3df.mp4"
+},
+{
+nome:"pantera negra",
+link:"https://download2391.mediafire.com/7sqinl47qw7g0jES-1N5RwWgktGsYDiQ4j5QxIRm0E9R_Msn9usYT7hu_-SiTnk1_qSHtXMjRFB-dvRm_wDcvRed_S3wTnj2hiJTVnsCKRaLuTbYESztEF4ciqcsLm0KDBo4N0lDDFKlscdRYCPMDBdrjw1o5V2t5RzCl6sDn8EylTE/1hl1wddqee4gxr2/efd88a7be576c3f1627c493984373be3.mp4"
 },
 
 {
