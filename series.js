25b3564f29dcda59944aef946229b07bca43fcfe
jukio98 <113953323+jukio98@users.noreply.github.com>
2024-09-18 12:26:04 -0300

    Atualizar o series.js

--- series.js
+++ series.js
@@ -1,5 +1,9 @@
 series=[
 {
+nome:"ESPN",
+link:"https://stream.embedflix.co/espn/tracks-v1a1/mono.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9OS8xOC8yMDI0IDE6MDc6MTAgQU0maGFzaF92YWx1ZT1QWkR4WFRwaU9BV0d2NVp5SklTTFh3PT0mdmFsaWRtaW51dGVzPTE2MA=="
+},
+{
 nome:"Acolyte - T1- ep1",
 link:"https://cdn-premium-01.lol/001/2eb7cad16e458ac5d78de048dc50279f.mp4"
 },
